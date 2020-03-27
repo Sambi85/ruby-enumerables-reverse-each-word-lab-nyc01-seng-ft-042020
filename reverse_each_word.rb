a11b05c5d6b1d699efc9f3b06eb49fdb89f90e80
Samuel Lesser <ineedmoreplates@gmail.com>
2020-03-27 04:07:54 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 
 def reverse_each_word(sentence1)
-sentence1.each_slice(1) do |flipped| puts sentence1.reverse
+pp sentence1.each_slice(1) do |flipped| puts sentence1.reverse
 end
-pp sentence1
+
 end
