db77ad2f9c3637454a3ace806ac55502ae315215
Samuel Lesser <ineedmoreplates@gmail.com>
2020-03-27 04:47:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@
 def reverse_each_word(sentence1)
 i = 0
 sentence_array = sentence1.split
-sentence_array.map do |word| sentence_array.reverse
+sentence_array.map do |word| word.reverse
 end
-pp sentence_array
+
 end
